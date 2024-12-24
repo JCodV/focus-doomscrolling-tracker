123deac8a5b413640436be6aae4603593d6c3fab
JCodV <154752793+JCodV@users.noreply.github.com>
2024-12-24 01:36:44 -0500

    added some comments for planning on future methods

--- focus.js
+++ focus.js
@@ -3,7 +3,6 @@ let trackedWebsites = [];
 function addTrackedWebsite() {
     const website = document.getElementById("website-to-track").value;
 
-
     if (!trackedWebsites.includes(website) && website.trim() != "") {
         trackedWebsites.push(website);
         addWebsiteToList(website);
@@ -18,4 +17,24 @@ function addWebsiteToList(website) {
     let li = document.createElement("li");
     li.textContent = website;
     ulTrackedWebsites.appendChild(li);
+}
+
+async function getCurrentTab() {
+    let queryOptions = { active: true, lastFocusedWindow: true };
+    let [tab] = await chrome.tabs.query(queryOptions);
+    console.log(tab);
+    return tab;
+}
+
+function isDoomscrollingWebsite(tab) {
+    let isDoomscroller = false;
+    let currUrlTrimmed = trimWebsiteUrl(tab.url);
+    
+}
+
+// use for tracked website and current tab url
+function trimWebsiteUrl(url) {
+    // remove https www .com .net etc.
+    // ex: https://www.reddit.com/ ----> reddit
+    // return string
 }
